922a2efbf977d3a530a6cb4d5dbfcc6af044dc75
Peibo Li <49071801+neolifer@users.noreply.github.com>
2021-06-08 07:26:42 +1000

    Add files via upload

--- examples/pytorch/gnn_explainer/models.py
+++ examples/pytorch/gnn_explainer/models.py
@@ -2,8 +2,79 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
+import torch
+import math
+import numpy as np
+from torch.nn.parameter import Parameter
 import dgl
 import dgl.function as fn
+from dgl.nn import GATConv
+
+class GraphConvolution(nn.Module):
+    def __init__(self, in_features, out_features, residual=False, variant=False):
+        super(GraphConvolution, self).__init__()
+        self.variant = variant
+        if self.variant:
+            self.in_features = 2*in_features
+        else:
+            self.in_features = in_features
+
+        self.out_features = out_features
+        self.residual = residual
+        self.weight = Parameter(torch.FloatTensor(self.in_features,self.out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.out_features)
+        self.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, graph, n_features , h0 , lamda, alpha, l, e_weights = None):
+        theta = math.log(lamda/l+1)
+        graph.ndata['h'] = n_features
+        if e_weights == None:
+            graph.update_all(fn.copy_u('h', 'm'), fn.mean('m', 'h'))
+        else:
+            graph.edata['ew'] = e_weights
+            graph.update_all(fn.u_mul_e('h', 'ew', 'm'), fn.mean('m', 'h'))
+        hi = graph.ndata['h']
+        if self.variant:
+            support = torch.cat([hi,h0],1)
+            r = (1-alpha)*hi+alpha*h0
+        else:
+            support = (1-alpha)*hi+alpha*h0
+            r = support
+        output = theta*torch.mm(support, self.weight)+(1-theta)*r
+        graph.ndata['h'] = output
+        return output
+
+class GCNII(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha, variant):
+        super(GCNII, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.params1 = list(self.convs.parameters())
+        self.params2 = list(self.fcs.parameters())
+        self.act_fn = nn.ReLU()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+
+    def forward(self,graph, x,  e_weights = None):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(graph, layer_inner, _layers[0],self.lamda,self.alpha,i+1, e_weights))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.fcs[-1](layer_inner)
+        return layer_inner
+
 
 
 class dummy_layer(nn.Module):
@@ -55,4 +126,44 @@ class dummy_gnn_model(nn.Module):
         h = self.out_layer(graph, h, edge_weights)
 
         return h
-        
+
+class GAT(nn.Module):
+    def __init__(self,
+                 g,
+                 num_layers,
+                 in_dim,
+                 num_hidden,
+                 num_classes,
+                 heads,
+                 activation,
+                 feat_drop,
+                 attn_drop,
+                 negative_slope,
+                 residual):
+        super(GAT, self).__init__()
+        self.g = g
+        self.num_layers = num_layers
+        self.gat_layers = nn.ModuleList()
+        self.activation = activation
+        # input projection (no residual)
+        self.gat_layers.append(GATConv(
+            in_dim, num_hidden, heads[0],
+            feat_drop, attn_drop, negative_slope, False, self.activation))
+        # hidden layers
+        for l in range(1, num_layers):
+            # due to multi-head, the in_dim = num_hidden * num_heads
+            self.gat_layers.append(GATConv(
+                num_hidden * heads[l-1], num_hidden, heads[l],
+                feat_drop, attn_drop, negative_slope, residual, self.activation))
+        # output projection
+        self.gat_layers.append(GATConv(
+            num_hidden * heads[-2], num_classes, heads[-1],
+            feat_drop, attn_drop, negative_slope, residual, None))
+
+    def forward(self, inputs):
+        h = inputs
+        for l in range(self.num_layers):
+            h = self.gat_layers[l](self.g, h).flatten(1)
+        # output projection
+        logits = self.gat_layers[-1](self.g, h).mean(1)
+        return logits
--- examples/pytorch/gnn_explainer/train_main.py
+++ examples/pytorch/gnn_explainer/train_main.py
@@ -4,14 +4,18 @@
 import os
 import argparse
 import dgl
-
+import torch
 import torch as th
 import torch.nn as nn
-
+import torch.nn.functional as F
 from dgl import save_graphs
-from models import dummy_gnn_model
+from models import dummy_gnn_model, GCNII, GAT
 from gengraph import gen_syn1, gen_syn2, gen_syn3, gen_syn4, gen_syn5
 import numpy as np
+from tqdm.notebook import tqdm
+import random
+import sys
+
 
 def main(args):
     # load dataset
@@ -27,7 +31,10 @@ def main(args):
         g, labels, name = gen_syn5()
     else:
         raise NotImplementedError
-    
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
     #Transform to dgl graph. 
     graph = dgl.from_networkx(g) 
     labels = th.tensor(labels, dtype=th.long)
@@ -43,16 +50,28 @@ def main(args):
     n_feats = graph.ndata['feat']
 
     #create model
-    dummy_model = dummy_gnn_model(args.feat_dim, args.hidden_dim, num_classes)
+    dummy_model =      model = GAT(graph,
+                                   args.num_layers,
+                                   args.feat_dim,
+                                   args.num_hidden,
+                                   num_classes,
+                                   ([args.num_heads] * args.num_layers) + [args.num_out_heads],
+                                   F.elu,
+                                   args.in_drop,
+                                   args.attn_drop,
+                                   args.negative_slope,
+                                   args.residual)
     loss_fn = nn.CrossEntropyLoss()
-    optim = th.optim.Adam(dummy_model.parameters(), lr=args.lr, weight_decay=args.wd)
+    optim = th.optim.Adam(dummy_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
 
     # train and output
-    for epoch in range(args.epochs):
+    for epoch in tqdm(range(args.epochs)):
 
         dummy_model.train()
 
         logits = dummy_model(graph, n_feats)
+
         loss = loss_fn(logits, labels)
         acc = th.sum(logits.argmax(dim=1) == labels).item() / len(labels)
         
@@ -73,10 +92,51 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='syn1', help='The dataset used for training the model.')
     parser.add_argument('--feat_dim', type=int, default=10, help='The feature dimension.')
     parser.add_argument('--hidden_dim', type=int, default=40, help='The hidden dimension.')
-    parser.add_argument('--epochs', type=int, default=500, help='The number of epochs.')
-    parser.add_argument('--lr', type=float, default=0.001, help='The learning rate.')
-    parser.add_argument('--wd', type=float, default=0.0, help='Weight decay.')
-
+    #hyper parameters for GCNII
+    parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+    # parser.add_argument('--epochs', type=int, default=1000, help='Number of epochs to train.')
+    # parser.add_argument('--lr', type=float, default=0.005, help='learning rate.')
+    # parser.add_argument('--wd1', type=float, default=0.0, help='weight decay (L2 loss on parameters).')
+    # parser.add_argument('--wd2', type=float, default=0, help='weight decay (L2 loss on parameters).')
+    # parser.add_argument('--layer', type=int, default=6, help='Number of layers.')
+    # parser.add_argument('--hidden', type=int, default=128, help='hidden dimensions.')
+    # parser.add_argument('--dropout', type=float, default=0, help='Dropout rate (1 - keep probability).')
+    # parser.add_argument('--patience', type=int, default=100, help='Patience')
+    # parser.add_argument('--data', default='cora', help='dateset')
+    # parser.add_argument('--dev', type=int, default=0, help='device id')
+    # parser.add_argument('--alpha', type=float, default=0.1, help='alpha_l')
+    # parser.add_argument('--lamda', type=float, default=0.5, help='lamda.')
+    # parser.add_argument('--variant', action='store_true', default=False, help='GCN* model.')
+    # parser.add_argument('--test', action='store_true', default=False, help='evaluation on test set.')
+    #hyper parameters for GAT
+    parser.add_argument("--gpu", type=int, default=-1,
+                        help="which GPU to use. Set -1 to use CPU.")
+    parser.add_argument("--epochs", type=int, default=1000,
+                        help="number of training epochs")
+    parser.add_argument("--num-heads", type=int, default=8,
+                        help="number of hidden attention heads")
+    parser.add_argument("--num-out-heads", type=int, default=1,
+                        help="number of output attention heads")
+    parser.add_argument("--num-layers", type=int, default=2,
+                        help="number of hidden layers")
+    parser.add_argument("--num-hidden", type=int, default=16,
+                        help="number of hidden units")
+    parser.add_argument("--residual", action="store_true", default=False,
+                        help="use residual connection")
+    parser.add_argument("--in-drop", type=float, default=0,
+                        help="input feature dropout")
+    parser.add_argument("--attn-drop", type=float, default=0,
+                        help="attention dropout")
+    parser.add_argument("--lr", type=float, default=0.005,
+                        help="learning rate")
+    parser.add_argument('--weight-decay', type=float, default=0,
+                        help="weight decay")
+    parser.add_argument('--negative-slope', type=float, default=0.2,
+                        help="the negative slope of leaky relu")
+    parser.add_argument('--early-stop', action='store_true', default=False,
+                        help="indicates whether to use early stop or not")
+    parser.add_argument('--fastmode', action="store_true", default=False,
+                        help="skip re-evaluate the validation set")
     args = parser.parse_args()
     print(args)
 
